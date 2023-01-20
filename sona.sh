8b4fbfdffe37bf866e6d22e376efbb4020a5f6cb
essiendaniel33 <essiendaniel33@gmail.com>
2023-01-20 12:48:59 +0000

    sona

---
+++ sona.sh
@@ -0,0 +1,9 @@
+! /bin/sh
+
+#
+# Copyright (c) 1999, 2006 Tanuki Software Inc.
+#
+# Java Service Wrapper sh script.  Suitable for starting and stopping
+#  wrapped Java applications on UNIX platforms.
+#
+"sonar.sh" 608L, 16393C                                                                                 1,1           Top
